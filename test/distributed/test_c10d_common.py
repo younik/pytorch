a56aa97b286377836d1ad986b5280db4e539ce60
Shen Li <cs.shenli@gmail.com>
2022-03-30 20:13:35 -0700

    [WIP] Fix SyncBatchNorm for empty inputs
    ghstack-source-id: a82e2dc1aef70696b9d6f63dd01071f87a1edc0b
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/74944

--- test/distributed/test_c10d_common.py
+++ test/distributed/test_c10d_common.py
@@ -806,6 +806,34 @@ class CommonDistributedDataParallelTest(object):
 
         return fut.then(fut_then)
 
+    @skip_if_lt_x_gpu(2)
+    def test_sync_batch_norm_empty_input(self):
+        pg = self._get_process_group()
+
+        model = torch.nn.Sequential(
+            torch.nn.BatchNorm2d(2),
+        ).to(device=self.rank)
+        model = DistributedDataParallel(
+            model,
+            device_ids=[self.rank],
+            process_group=pg,
+        )
+        model = torch.nn.SyncBatchNorm.convert_sync_batchnorm(
+            model,
+            process_group=pg,
+        )
+
+        model.train()
+        x = torch.zeros((1 if self.rank != 0 else 0, 2, 11, 13), dtype=torch.float32, device=self.rank)
+        #x = torch.zeros((2, 2, 11, 13), dtype=torch.float32, device=self.rank)
+        x.requires_grad = True
+        y = model(x)
+        y.sum().backward()
+
+        x.requires_grad = False
+        y = model(x)
+        y.sum().backward()
+
 
 class ComputeBucketAssignmentTest(TestCase):
     def test_single_limit_single_dtype(self):
--- torch/nn/modules/_functions.py
+++ torch/nn/modules/_functions.py
@@ -1,3 +1,4 @@
+from hamcrest import none
 import torch
 import torch.distributed as dist
 
@@ -16,17 +17,25 @@ class SyncBatchNorm(Function):
         if size == 1 and world_size < 2:
             raise ValueError('Expected more than 1 value per channel when training, got input size {}'.format(size))
 
-        # calculate mean/invstd for input.
-        mean, invstd = torch.batch_norm_stats(input, eps)
-
-        count = torch.full((1,), input.numel() // input.size(1),
-                           dtype=mean.dtype,
-                           device=mean.device)
-
-
         num_channels = input.shape[1]
-        # C, C, 1 -> (2C + 1)
-        combined = torch.cat([mean, invstd, count], dim=0)
+        if input.numel() > 0:
+            # calculate mean/invstd for input.
+            mean, invstd = torch.batch_norm_stats(input, eps)
+
+            count = torch.full((1,), input.numel() // input.size(1),
+                            dtype=mean.dtype,
+                            device=mean.device)
+
+            # C, C, 1 -> (2C + 1)
+            combined = torch.cat([mean, invstd, count], dim=0)
+        else:
+            # for empty input, directly set all stats to 0
+            combined = torch.zeros(
+                2 * num_channels + 1,
+                dtype=input.dtype,
+                device=input.device
+            )
+
         # Use allgather instead of allreduce because count could be different across
         # ranks, simple all reduce op can not give correct results.
         # batch_norm_gather_stats_with_counts calculates global mean & invstd based on
@@ -53,6 +62,12 @@ class SyncBatchNorm(Function):
             # world_size * (2C + 1) -> world_size * C, world_size * C, world_size * 1
             mean_all, invstd_all, count_all = torch.split(combined, num_channels, dim=1)
 
+        # remove stats from empty inputs
+        mask = count_all.squeeze(-1) >= 1
+        count_all = count_all[mask]
+        mean_all = mean_all[mask]
+        invstd_all = invstd_all[mask]
+
         # calculate global mean & invstd
         mean, invstd = torch.batch_norm_gather_stats_with_counts(
             input,
@@ -69,8 +84,10 @@ class SyncBatchNorm(Function):
         self.process_group = process_group
 
         # apply element-wise normalization
-        out = torch.batch_norm_elemt(input, weight, bias, mean, invstd, eps)
-        return out
+        if input.numel() > 0:
+            return torch.batch_norm_elemt(input, weight, bias, mean, invstd, eps)
+        else:
+            return torch.empty_like(input)
 
     @staticmethod
     def backward(self, grad_output):
@@ -80,45 +97,73 @@ class SyncBatchNorm(Function):
         grad_input = grad_weight = grad_bias = None
         process_group = self.process_group
 
-        # calculate local stats as well as grad_weight / grad_bias
-        sum_dy, sum_dy_xmu, grad_weight, grad_bias = torch.batch_norm_backward_reduce(
-            grad_output,
-            saved_input,
-            mean,
-            invstd,
-            weight,
-            self.needs_input_grad[0],
-            self.needs_input_grad[1],
-            self.needs_input_grad[2]
-        )
-
-        if self.needs_input_grad[0]:
-            # synchronizing stats used to calculate input gradient.
-            num_channels = sum_dy.shape[0]
-            combined = torch.cat([sum_dy, sum_dy_xmu], dim=0)
-            torch.distributed.all_reduce(
-                combined, torch.distributed.ReduceOp.SUM, process_group, async_op=False)
-            sum_dy, sum_dy_xmu = torch.split(combined, num_channels)
-
-            # backward pass for gradient calculation
-            grad_input = torch.batch_norm_backward_elemt(
+        if saved_input.numel() > 0:
+            # calculate local stats as well as grad_weight / grad_bias
+            sum_dy, sum_dy_xmu, grad_weight, grad_bias = torch.batch_norm_backward_reduce(
                 grad_output,
                 saved_input,
                 mean,
                 invstd,
                 weight,
-                sum_dy,
-                sum_dy_xmu,
-                count_tensor
+                self.needs_input_grad[0],
+                self.needs_input_grad[1],
+                self.needs_input_grad[2]
             )
 
-        # synchronizing of grad_weight / grad_bias is not needed as distributed
-        # training would handle all reduce.
-        if weight is None or not self.needs_input_grad[1]:
-            grad_weight = None
+            if self.needs_input_grad[0]:
+                # synchronizing stats used to calculate input gradient.
+                num_channels = sum_dy.shape[0]
+                combined = torch.cat([sum_dy, sum_dy_xmu], dim=0)
+                torch.distributed.all_reduce(
+                    combined, torch.distributed.ReduceOp.SUM, process_group, async_op=False)
+                sum_dy, sum_dy_xmu = torch.split(combined, num_channels)
 
-        if weight is None or not self.needs_input_grad[2]:
-            grad_bias = None
+                # backward pass for gradient calculation
+                grad_input = torch.batch_norm_backward_elemt(
+                    grad_output,
+                    saved_input,
+                    mean,
+                    invstd,
+                    weight,
+                    sum_dy,
+                    sum_dy_xmu,
+                    count_tensor
+                )
+            # synchronizing of grad_weight / grad_bias is not needed as distributed
+            # training would handle all reduce.
+            if weight is None or not self.needs_input_grad[1]:
+                grad_weight = None
+
+            if weight is None or not self.needs_input_grad[2]:
+                grad_bias = None
+        else:
+            num_channels = saved_input.shape[1]
+            if self.needs_input_grad[0]:
+                # launch all_reduce to unblock other peer processes
+                combined = torch.zeros(
+                    2 * num_channels,
+                    dtype=saved_input.dtype,
+                    device=saved_input.device
+                )
+                torch.distributed.all_reduce(
+                    combined, torch.distributed.ReduceOp.SUM, process_group, async_op=False)
+
+                # input is empty, we can skip batch_norm_backward_elemt
+                grad_input = torch.zeros_like(saved_input)
+            else:
+                grad_input = None
+
+            grad_weight = torch.zeros(
+                num_channels,
+                dtype=saved_input.dtype,
+                device=saved_input.device
+            ) if weight is not None and self.needs_input_grad[1] else None
+
+            grad_bias= torch.zeros(
+                num_channels,
+                dtype=saved_input.dtype,
+                device=saved_input.device
+            ) if weight is not None and self.needs_input_grad[2] else None
 
         return grad_input, grad_weight, grad_bias, None, None, None, None, None, None
 
